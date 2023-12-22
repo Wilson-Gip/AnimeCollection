98f440d2a1eefe0526719d6bd0d132164a73150f
Wilson-Gip <wilsongip101@gmail.com>
2023-12-22 01:15:58 -0800

    animecollection infinite scrolling and framer motions added

---
+++ components/MotionDiv.tsx
@@ -0,0 +1,5 @@
+"use client";
+import  { motion } from "framer-motion";
+
+//client side render the motion div to keep the other parts of the animecard server sided rendering
+export const MotionDiv = motion.div;
